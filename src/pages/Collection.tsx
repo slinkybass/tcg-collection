ed417409c510463fb7f606be614aba2aabd81999
slinkybass <israel@garaballu.com>
2025-09-04 09:58:04 +0200

    Basic app structure

---
+++ src/pages/Collection.tsx
@@ -0,0 +1,3 @@
+export default function Collection() {
+  return <h1 className="text-2xl font-bold">Colección</h1>;
+}
---
+++ src/pages/Sets.tsx
@@ -0,0 +1,3 @@
+export default function Sets() {
+  return <h1 className="text-2xl font-bold">Sets</h1>;
+}
